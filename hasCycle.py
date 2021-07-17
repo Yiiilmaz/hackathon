30e941f8b5ab95713ea64702f6ab23b2f456eccb
Michael <michael2002lu@gmail.com>
2021-07-17 19:00:32 +1000

    Another version

--- hasCycle.py
+++ hasCycle.py
@@ -1,21 +1,37 @@
+'''hasCycle(G):
+|  Input  graph G
+|  Output true if G has a cycle, false otherwise
+|
+|  choose any vertex v ∈ G
+|  return dfsCycleCheck(G,v)
+
+dfsCycleCheck(G,v):
+|  mark v as visited
+|  for each (v,w) ∈ edges(G) do
+|  |  if w has been visited then  // found cycle
+|  |     return true
+|  |  else if dfsCycleCheck(G,w) then
+|  |     return true
+|  end for
+|  return false  // no cycle at v
+
+'''
+
 def hasCycle(index):
-    visited = [0] * index
+    visited = [-1] * index
     for v in range(index):
-        for u in range(index):
-            visited[u] = -1
-        if (dfsCycleCheck(index, v, v, visited)):
+        visited = [-1] * index
+        if (dfsCycleCheck(index, v, visited)):
             return True
+
     return False
 
-
-def dfsCycleCheck(index, v, u, visited):
-    visited[v] = False
+def dfsCycleCheck(index, v, visited):
+    visited[v] = True
     for w in range(index):
-        if ( m[v][w] == 0): # Most likely wrong
-                if not visited[w]:
-                    if dfsCycleCheck(index, v, u, visited):
-                        return True
-                elif (w != u):
-                    return True
+        if (m[v][w]):
+            if visited[w]:
+                return True
+            elif (dfsCycleCheck(index, w)):
+                return True
     return False
-
