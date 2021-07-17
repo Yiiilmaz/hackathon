e59159932e029097528d485885e0dd91c0a7143c
Alec Dudley-Bestow <z5260201@ad.unsw.edu.au>
2021-07-17 20:58:48 +1000

    finiished

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 #produce a list of connections
 
 import numpy as np
+import matplotlib.pyplot as plt
 import csv
 
 
@@ -15,48 +16,34 @@ def find_distance(town1, town2):
 def get_weight(edge):
     return edge['w']
 
-def isCyclicUtil(graph, v, visited, parent, size):
+def adjacent(G, v, w, size):
+    return (w != v and G[w][v] != -1)
 
-    # Mark the current node as visited
-    visited[v]= True
-    print(graph)
-    # Recur for all the vertices
-    # adjacent to this vertex
-    for i in range(size):
-        if (graph[v][i] != -1):
-            # If the node is not
-            # visited then recurse on it
-            if visited[i]==False :
-                if(isCyclicUtil(graph,i,visited,v,size)):
+
+def isCyclicUtil(graph, v, u, visited, size):
+    visited[v] = True
+    for w in range(size):
+        if (adjacent(graph, v, w, size)):
+            if not visited[w]:
+                if isCyclicUtil(graph, w, v, visited, size):
                     return True
-            # If an adjacent vertex is
-            # visited and not parent
-            # of current vertex,
-            # then there is a cycle
-            elif  parent!=i:
-                return True
-     
+            elif w != u:
+                return True;
+    
+
     return False
 
 def isCyclic(graph, size):
    
-    # Mark all the vertices
-    # as not visited
-    visited =[False]*(size)
-     
-    # Call the recursive helper
-    # function to detect cycle in different
-    # DFS trees
-    for i in range(1, size):
-        print(visited)
-        # Don't recur for u if it
-        # is already visited
-        if visited[i] == False:
-    
-            if(isCyclicUtil(graph,i,visited,0,size)) == True:
-                return True
-     
-    return False
+    result = False
+    visited = [False] * size
+    for v in range(size):
+        for i in range(size):
+            visited[i] = False
+        if isCyclicUtil(graph, v, v, visited, size):
+            result = True
+            break;
+    return result
     
 
 
@@ -64,6 +51,7 @@ def add_edge(G, edge):
     G[edge['i']][edge['j']] = edge['w']
     G[edge['j']][edge['i']] = edge['w']
     return G
+
 
 
 def KruskalMST(G, size):
@@ -74,13 +62,16 @@ def KruskalMST(G, size):
     num_edges = 0
     for i in range(size):
         for j in range(i, size):
-            if G[i][j] != -1:
+            if G[i][j] != -1 and i != j:
                 edge_list.append({'i' : i, 'j' : j, 'w' : G[i][j]})
     
     edge_list.sort(key=get_weight)
-    
+    print(edge_list)
     for edge in edge_list:
+        
+        
         MST = add_edge(MST, edge)
+        print(MST)
         print(isCyclic(MST, size))
         num_edges += 1
         #if theres a cycle drop the edge
@@ -91,6 +82,7 @@ def KruskalMST(G, size):
             MST = add_edge(MST, edge)
         if num_edges == size - 1:
             return MST
+    return MST
 
 
 towns = []
@@ -102,8 +94,8 @@ with open('towns.csv', newline='') as csvfile:
     for row in reader:
         towns.append({
         'name' : row[0],
-        'x' : int(row[1]),
-        'y' : int(row[2]),
+        'x' : float(row[1]),
+        'y' : float(row[2]),
         'index' : index,
         'node_type' : 'town'
         })
@@ -117,8 +109,8 @@ with open('nodes.csv', newline='') as csvfile:
         
         towns.append({
         'name' : row[0],
-        'x' : int(row[1]),
-        'y' : int(row[2]),
+        'x' : float(row[1]),
+        'y' : float(row[2]),
         'index' : index,
         'node_type' : 'node'
         })
@@ -130,10 +122,9 @@ m[:] = -1
 for town1 in towns:
     if town1['node_type'] == 'town':
         for town2 in towns:
-            if town2['node_type'] == 'town': 
                 
-                m[town1['index']][town2['index']] = find_distance(town1, town2)
-                m[town2['index']][town1['index']] = find_distance(town1, town2)
+            m[town1['index']][town2['index']] = find_distance(town1, town2)
+            m[town2['index']][town1['index']] = find_distance(town1, town2)
             
     if town1['node_type'] == 'node':
         for town2 in towns:
@@ -141,10 +132,23 @@ for town1 in towns:
                 m[town1['index']][town2['index']] = 0
                 m[town2['index']][town1['index']] = 0
     
-
+#print(m)
 MST = KruskalMST(m, index)
 print(MST)
-    
+
+xs = []
+ys = []
+for i in range(index):
+    for j in range(i, index):
+        if adjacent(MST, i, j, index) and MST[i][j] != 0:
+            
+            plt.plot([towns[i]['x'], towns[j]['x']], [towns[i]['y'], towns[j]['y']], 'y')
+
+
+plt.plot(   [town['x'] for town in towns if town['node_type'] == 'town'], [town['y'] for town in towns if town['node_type'] == 'town'], 'ro',
+            [town['x'] for town in towns if town['node_type'] == 'node'], [town['y'] for town in towns if town['node_type'] == 'node'], 'bs' ,
+     )
+plt.show()
 
 
         
